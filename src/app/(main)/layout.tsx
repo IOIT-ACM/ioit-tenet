b9c830b73e082e839ea123d3ffbab2147c323809
Aditya <adimail2404@gmail.com>
2024-08-03 20:08:56 +0530

    FEAT: React scroll parallax

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/common/footer';
 import FixedNavBar from '@/components/common/fixednav';
+import { Providers } from '@/modules/providers';
 
 interface SiteLayoutProps {
   children: React.ReactNode;
@@ -9,7 +10,9 @@ export default async function SiteLayout({ children }: SiteLayoutProps) {
   return (
     <div className='flex min-h-screen flex-col'>
       <FixedNavBar />
-      <main className='flex-1'>{children}</main>
+      <main className='flex-1'>
+        <Providers>{children}</Providers>
+      </main>
       <Footer />
     </div>
   );
