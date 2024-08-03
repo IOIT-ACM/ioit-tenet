598b2aa239e5a299f572def3259addfd4a998908
Aditya <adimail2404@gmail.com>
2024-08-03 12:57:43 +0530

    skip button for splash video

--- src/modules/home/components/heropage.tsx
+++ src/modules/home/components/heropage.tsx
@@ -10,17 +10,29 @@ export const Hero = () => {
     setVideoEnded(true);
   };
 
+  const handleSkip = () => {
+    setVideoEnded(true);
+  };
+
   return (
-    <div>
+    <div className='relative'>
       {!videoEnded ? (
-        <Video
-          src='hero-logo.webm'
-          autoPlay
-          preload='auto'
-          muted
-          onEnded={handleVideoEnded}
-          style={{ width: '100vw', height: '100vh', objectFit: 'cover' }}
-        />
+        <>
+          <Video
+            src='hero-logo.webm'
+            autoPlay
+            preload='auto'
+            muted
+            onEnded={handleVideoEnded}
+            style={{ width: '100vw', height: '100vh', objectFit: 'cover' }}
+          />
+          <button
+            onClick={handleSkip}
+            className='absolute bottom-4 right-4 rounded-md bg-black px-4 py-2 text-white shadow-lg'
+          >
+            Skip
+          </button>
+        </>
       ) : (
         <div>
           <h1 className='text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]'>
