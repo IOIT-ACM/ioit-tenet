273740fc4a96878e3f09d501a22ad3cedebb016f
Aditya <adimail2404@gmail.com>
2024-08-03 11:55:45 +0530

    tailwind typography, sitemap, modules structure and robots.txt file

---
+++ src/modules/home/components/heropage.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        IOIT <span className="text-[hsl(280,100%,70%)]">TENET</span>
+      </h1>
+    </div>
+  );
+};
