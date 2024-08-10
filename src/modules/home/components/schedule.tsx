5ca17879a29c531ead914e9df928327f5595350f
Aditya <adimail2404@gmail.com>
2024-08-10 11:51:25 +0530

    Update schedule.tsx

--- src/modules/home/components/schedule.tsx
+++ src/modules/home/components/schedule.tsx
@@ -5,28 +5,80 @@ import { FiMapPin } from 'react-icons/fi';
 
 export const Schedule = () => {
   return (
-    <section className='mx-auto max-w-5xl px-4 py-48 text-white'>
+    <section className='relative mx-auto grid max-w-5xl grid-cols-1 justify-center text-white transition-all md:grid-cols-5'>
       <motion.h1
         initial={{ y: 48, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ ease: 'easeInOut', duration: 0.75 }}
-        className='mb-20 text-4xl font-black uppercase text-zinc-50 md:text-7xl'
+        className='sticky top-10 col-span-5 mb-20 w-full text-4xl font-black uppercase text-zinc-50 md:col-span-2 md:text-5xl'
       >
         Schedule
       </motion.h1>
-      <ScheduleItem
-        title='Techfiesta'
-        date='Oct 4th'
-        location='4th floor 405'
-      />
-      <ScheduleItem title='MUN' date='Oct 4th' location='6th Floor 609' />
-      <ScheduleItem title='E-Summit' date='Oct 4th' location='4th floor 405' />
-      <ScheduleItem
-        title='Creators Conclave'
-        date='Oct 4th'
-        location='4th floor 405'
-      />
-      <ScheduleItem title='E-Sports' date='Oct 4th' location='1st Floor 112' />
+      <div className='w-full md:col-span-3'>
+        <ScheduleItem
+          title='Techfiesta'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem title='MUN' date='Oct 4th' location='6th Floor 609' />
+        <ScheduleItem
+          title='E-Summit'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem
+          title='Creators Conclave'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem
+          title='E-Sports'
+          date='Oct 4th'
+          location='1st Floor 112'
+        />
+        <ScheduleItem
+          title='Techfiesta'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem title='MUN' date='Oct 4th' location='6th Floor 609' />
+        <ScheduleItem
+          title='E-Summit'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem
+          title='Creators Conclave'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem
+          title='E-Sports'
+          date='Oct 4th'
+          location='1st Floor 112'
+        />
+        <ScheduleItem
+          title='Techfiesta'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem title='MUN' date='Oct 4th' location='6th Floor 609' />
+        <ScheduleItem
+          title='E-Summit'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem
+          title='Creators Conclave'
+          date='Oct 4th'
+          location='4th floor 405'
+        />
+        <ScheduleItem
+          title='E-Sports'
+          date='Oct 4th'
+          location='1st Floor 112'
+        />
+      </div>
     </section>
   );
 };
