16365a5dc3698be6f60c69dc65301c6ac989b14f
swarooppatilx <swarooppatil850@gmail.com>
2024-08-24 17:45:42 +0530

    FEAT: Creators Conclave Page, moved sponsors to components/common

--- src/app/(tenet)/creators/page.tsx
+++ src/app/(tenet)/creators/page.tsx
@@ -1,7 +1,21 @@
-import CommingSoon from '@/components/commingsoon';
+import {
+  HeroImage,
+  Timeline,
+  Sponsors,
+} from '@/modules/events/creators';
 
-const Page = () => {
-  return <CommingSoon />;
-};
-
-export default Page;
+export default function Page() {
+  return (
+    <main>
+      <HeroImage
+        backgroundImage='https://images.unsplash.com/photo-1682687982360-3fbab65f9d50?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        title='Welcome to Creators Conclave'
+        subtitle='Join us for an exciting event'
+        ctaText='Learn More'
+        ctaLink='#timeline'
+      />
+      <Timeline domain='creators' />
+      <Sponsors />
+    </main>
+  );
+}
--- src/modules/events/creators/index.tsx
+++ src/modules/events/creators/index.tsx
@@ -1,4 +1,3 @@
 export { HeroImage } from '@/components/common/heroimage'
-export { Speakers } from './components/speakers';
 export { Timeline } from '@/components/common/timeline';
 export { Sponsors } from '@/components/common/sponsors';
