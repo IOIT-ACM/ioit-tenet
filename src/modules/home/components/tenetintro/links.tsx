a73a7094015a246d2ea753360cc6ec5cbd9e280b
adimail <adimail2404@gmail.com>
2024-09-17 14:54:49 +0530

    magnetic links

--- src/modules/home/components/tenetintro/links.tsx
+++ src/modules/home/components/tenetintro/links.tsx
@@ -1,18 +1,23 @@
-import Link from 'next/link';
+import { KONFHUB_PAGE } from '@/config';
+import { Magnets } from '@/components/ui/RoundedButton/magnets';
 
 export const Links = () => {
   return (
-    <div className='absolute bottom-0 h-fit transform md:right-5 md:top-1/2 md:-translate-y-1/2'>
+    <div className='absolute bottom-0 h-fit transform md:right-10 md:top-1/2 md:-translate-y-1/2'>
       <div className='flex w-screen select-none items-center justify-center gap-10 text-center md:w-full md:flex-col md:text-2xl'>
-        <Link href='register' className='text-white md:mt-0'>
+        <Magnets
+          link={KONFHUB_PAGE}
+          newpage
+          className='bg-none text-white md:mt-0'
+        >
           Early bird pass
-        </Link>
-        <Link href='events' className='text-white md:mt-0'>
+        </Magnets>
+        <Magnets link='/events' className='text-white md:mt-0'>
           Events
-        </Link>
-        <Link href='speakers' className='text-white md:mt-0'>
+        </Magnets>
+        <Magnets link='/speakers' className='text-white md:mt-0'>
           Speakers
-        </Link>
+        </Magnets>
       </div>
     </div>
   );
