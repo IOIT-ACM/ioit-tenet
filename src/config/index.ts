66d323a60e8a5172f1317a3311225b146fec4c77
adimail <adimail2404@gmail.com>
2024-09-20 20:50:09 +0530

    Update MUN register link

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,6 @@ export const siteConfig = {
   url: 'https://www.ioittenet.com/',
 };
 
-export const MUNLINK = 'https://forms.gle/W11goEvziKFYYXqz5';
+export const MUNLINK = 'https://forms.gle/Yhzr1nftHeL18Jaf6';
 export const KONFHUB_PAGE = 'https://konfhub.com/tenet2024';
 export const LOADTIME = 2.8;
