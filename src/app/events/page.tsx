23f7fc274c50051c8bf527de4ac8d2466447c825
adimail <adimail2404@gmail.com>
2024-09-28 20:53:41 +0530

    Events page updates

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -9,6 +9,16 @@ export const metadata: Metadata = {
   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
   title: 'Events at TENET',
   description: 'Explore the events happening at IOIT TENET 2024',
+  openGraph: {
+    images: [
+      {
+        url: 'https://hosteze-little-boy.s3.ap-south-1.amazonaws.com/assets/static/tenet/events/events-og.jpeg',
+        width: 800,
+        height: 600,
+        alt: `Events in TENET 2024`,
+      },
+    ],
+  },
 };
 
 const Page = () => {
