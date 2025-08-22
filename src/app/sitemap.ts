3ebf76bb442e4ac325c3c44424946359948671ae
Sarvesh Kolte <renarinkholin4@proton.me>
2025-08-22 23:47:32 +0530

    feat: Add schedule page and update links and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { siteConfig } from '@/config';
 import { type MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ['', 'team', 'events'].map((route) => ({
+  const routes = ['', 'team', 'events', 'schedule'].map((route) => ({
     url: `${siteConfig.url}${route}`,
     lastModified: new Date().toISOString(),
   }));
