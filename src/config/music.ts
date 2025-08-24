75a07e44c13798579beb19542742aec52cc42145
IOIT ACM Webmaster <acm.studentbranch@aissmsioit.org>
2025-08-24 22:42:02 +0530

    fix: music routes (#99)
    Co-authored-by: adimail <adimail2404@gmail.com>

--- src/config/music.ts
+++ src/config/music.ts
@@ -5,31 +5,30 @@ export const musicConfig: MusicConfig[] = [
     route: '/',
     music: [
       'https://ioit.acm.org/tenet/tracks/CYBERPUNK2077.mp3',
-      'https://ioit.acm.org/tenet/tracks/hero.mp3',
-      'https://ioit.acm.org/tenet/tracks/hero2.mp3',
+      'https://ioit.acm.org/tenet/tracks/greenbagboogie.mp3'
     ],
   },
   {
-    route: '/techfiesta',
+    route: '/24/techfiesta',
     music: [
       'https://ioit.acm.org/tenet/tracks/hero.mp3',
       'https://ioit.acm.org/tenet/tracks/hero2.mp3',
     ],
   },
   {
-    route: '/esports',
+    route: '/24/esports',
     music: ['https://ioit.acm.org/tenet/tracks/CYBERPUNK2077.mp3'],
   },
   {
-    route: '/mun',
+    route: '/24/mun',
     music: ['https://ioit.acm.org/tenet/tracks/cornfieldchase.mp3'],
   },
   {
-    route: '/esummit',
+    route: '/24/esummit',
     music: ['https://ioit.acm.org/tenet/tracks/greenbagboogie.mp3'],
   },
   {
-    route: '/creators',
+    route: '/24/creators',
     music: ['https://ioit.acm.org/tenet/tracks/tambdi-chambdi.mp3'],
   },
 ];
