1584ed96b47c24c5a3f7fbb61bc918791b7f5f30
adimail <adimail2404@gmail.com>
2025-09-14 16:18:39 +0530

    setup: techfiesta page and slugs layout built for sarvesh to work hehe

---
+++ src/app/techfiesta/[event]/page.tsx
@@ -0,0 +1,9 @@
+//import data from cpnfig/25/techfiesta
+
+export default function Techfiesta() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen px-4 bg-gray-950 text-white">
+            Dynamic page for each event will be rendered here.
+        </div>
+    ); 
+}
