9f7eae87cbeaa316b3917e1f667b847b406ebe26
swarooppatilx <swarooppatil850@gmail.com>
2025-09-30 12:07:08 +0530

    chore: update esports qr

--- src/config/data/25/navbar.ts
+++ src/config/data/25/navbar.ts
@@ -4,4 +4,5 @@ export const routes: navbarType[] = [
   { path: '/techfiesta', name: 'Techfiesta' },
   { path: 'https://mun.ioittenet.com/', name: 'MUN' },
   { path: 'https://hack.ioittenet.com/', name: 'Hackathon' },
+  { path: '/register/esports', name: 'eSports' },
 ];
